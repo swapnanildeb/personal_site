fd6b5543f12245de65cddcc9edd460a280560e65
swapnanil deb <swapnanildeb3@gmail.com>
2020-08-27 13:23:01 -0400

    data changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ class Header extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
+      var nickname = this.props.data.nickname;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
@@ -34,7 +35,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">I'm {nickname}.</h1>
             <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
